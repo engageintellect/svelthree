d6528cd1f37373a1e815c885ba905b7958af46b8
engageintellect <jc9361@gmail.com>
2023-03-25 23:22:17 -0700

    init

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
-  import "../app.css";
+	import '../app.css';
 </script>
 
-<slot />
+<div class="h-screen flex justify-center items-center max-w-5xl mx-auto px-2 lg:px-4">
+	<slot />
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,42 +1,15 @@
 <script lang="ts">
-	import {
-		Canvas,
-		Scene,
-		PerspectiveCamera,
-		DirectionalLight,
-		AmbientLight,
-		BoxBufferGeometry,
-		Mesh,
-		MeshStandardMaterial,
-		WebGLRenderer
-	} from 'https://unpkg.com/svelthree@latest/dist/svelthree.mjs';
-
-	let cubeGeometry = new BoxBufferGeometry(1, 1, 1);
-	let cubeMaterial = new MeshStandardMaterial();
+	import SpinningCube from '../components/SpinningCube.svelte';
 </script>
 
-<div class="flex flex-col lg:flex-row justify-center items-center">
+<div class="flex flex-col gap-2 lg:flex-row items-center">
 	<div>
-		<div class="text-7xl">Hello <br />from Svelthree.</div>
+		<div class="text-7xl">
+			hello<br /><a target="_blank" href="https://svelthree.dev/">svelthree</a>
+		</div>
 	</div>
 
-	<Canvas let:sti w={500} h={500}>
-		<Scene {sti} let:scene id="scene1" props={{ background: 0xffffff }}>
-			<PerspectiveCamera {scene} id="cam1" pos={[0, 0, 3]} lookAt={[0, 0, 0]} />
-			<AmbientLight {scene} intensity={1.25} />
-			<DirectionalLight {scene} pos={[3, 3, 3]} />
-
-			<Mesh
-				{scene}
-				geometry={cubeGeometry}
-				material={cubeMaterial}
-				mat={{ roughness: 0.5, metalness: 0.5, color: 0x14b8a6 }}
-				pos={[0, 0, 0]}
-				rot={[0.5, 0.75, 0]}
-				scale={[1, 1, 1]}
-			/>
-		</Scene>
-
-		<WebGLRenderer {sti} sceneId="scene1" camId="cam1" config={{ antialias: true, alpha: true }} />
-	</Canvas>
+	<div>
+		<SpinningCube />
+	</div>
 </div>
